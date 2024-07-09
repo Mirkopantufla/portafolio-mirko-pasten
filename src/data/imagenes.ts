2db5f60e4b2f8fbe6604daa05b21b21932eca13f
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2024-07-08 20:22:46 -0400

    Agregada seccion de proyectos, y estructurado un poco el proyecto, no se ha terminado, se guardan avances

---
+++ src/data/imagenes.ts
@@ -0,0 +1,9 @@
+export const battleship: string[] = ["battleship/battleship-1.JPG", "battleship/battleship-2.JPG", "battleship/battleship-3.JPG", "battleship/battleship-4.JPG", "battleship/battleship-4.JPG"]
+
+export const inmuebles_miki: string[] = ["inmuebles_miki/inmuebles_miki-1.JPG", "inmuebles_miki/inmuebles_miki-2.JPG", "inmuebles_miki/inmuebles_miki-3.JPG", "inmuebles_miki/inmuebles_miki-4.JPG", "inmuebles_miki/inmuebles_miki-5.JPG", "inmuebles_miki/inmuebles_miki-6.JPG"]
+
+export const only_flans: string[] = ["only_flans/only_flans-1.JPG", "only_flans/only_flans-2.JPG", "only_flans/only_flans-3.JPG", "only_flans/only_flans-4.JPG"]
+
+export const pajaros_chilenos: string[] = ["pajaros_chilenos/pajaros_chilenos-1.JPG", "pajaros_chilenos/pajaros_chilenos-2.JPG", "pajaros_chilenos/pajaros_chilenos-3.JPG", "pajaros_chilenos/pajaros_chilenos-4.JPG"]
+
+export const tic_tac_toe: string[] = ["tic_tac/tic_tac-1.JPG", "tic_tac/tic_tac-2.JPG", "tic_tac/tic_tac-3.JPG", "tic_tac/tic_tac-4.JPG"]
