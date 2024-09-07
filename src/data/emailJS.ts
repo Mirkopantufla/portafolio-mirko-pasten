d62e93909ac86ebe8c2abec4107dc44fb77ae2b6
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2024-09-07 10:33:05 -0400

    Cambiado lugar de importacion de archivos ts, para solucionar problemas de vercel

--- src/data/emailJS.ts
+++ src/data/emailJS.ts
@@ -1,44 +1,48 @@
 import { EMAILJS_SERVICE, EMAILJS_TEMPLATE, EMAILJS_PK } from 'astro:env/client'
 import { emailRegex, nameRegex } from './regex';
+import emailjs from '@emailjs/browser';
 
 emailjs.init(EMAILJS_PK);
 
-const btn = document.getElementById('buttonForm');
-const form = document.getElementById('contactForm')
+const btn = document.getElementById('buttonForm') as HTMLButtonElement;
+const form = document.getElementById('contactForm') as HTMLFormElement;
 
 function validateData() {
   let error = false;
-  const from_name = document.getElementById("from_name");
-  const message_id = document.getElementById("message_id");
-  const message = document.getElementById("message");
+  const from_name = document.getElementById("from_name") as HTMLInputElement;
+  let from_name_error = from_name.nextElementSibling?.innerHTML as string
+  const message_id = document.getElementById("message_id") as HTMLInputElement;
+  let message_id_error = from_name.nextElementSibling?.innerHTML as string
+  const message = document.getElementById("message") as HTMLInputElement;
+  let message_error = from_name.nextElementSibling?.innerHTML as string
 
   if (from_name.value === "") {
-    from_name.nextElementSibling.innerHTML = "El nombre no puede quedar vacio"
+    from_name_error = "El nombre no puede quedar vacio"
     error = true;
   }else if(!nameRegex.test(from_name.value)){
-    from_name.nextElementSibling.innerHTML = "Debe ser un nombre valido"
+    from_name_error = "Debe ser un nombre valido"
     error = true;
   }else{
-    from_name.nextElementSibling.innerHTML = ""
+    from_name_error = ""
     from_name.style.outline = "3px solid green"
   }
 
   if (message_id.value === "") {
-    message_id.nextElementSibling.innerHTML = "El email no puede quedar vacio"
+    message_id_error = "El email no puede quedar vacio"
     error = true;
   }else if(!emailRegex.test(message_id.value)){
-    message_id.nextElementSibling.innerHTML = "Debe ser un email valido"
+    message_id_error = "Debe ser un email valido"
     error = true;
   }else{
-    message_id.nextElementSibling.innerHTML = ""
+    message_id_error = ""
     message_id.style.outline = "3px solid green"
   }
 
   if (message.value === "") {
-    message.nextElementSibling.innerHTML = "El mensaje no puede quedar vacio"
+    message_error = "El mensaje no puede quedar vacio"
     error = true;
   }else{
-    message.nextElementSibling.innerHTML = ""
+    message_error = ""
     message.style.outline = "3px solid green"
   }
 
